fab557875204d6fb3d666f5554a088a3757c164a
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-06-27 14:30:46 +0500

    Refactor login page to improve UI consistency and readability

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -26,9 +26,9 @@ const Page: FC = () => {
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
-          <div className="flex flex-col items-center gap-8">
+          <div className="flex flex-col items-center gap-3">
             <Image alt="App Main Logo" src={AppLogo} />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
           </div>
